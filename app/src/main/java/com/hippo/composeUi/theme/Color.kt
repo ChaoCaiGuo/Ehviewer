95fcdf3e585c2b5c352b85c9fed1cc78cf89baae
Guo <heitao_8@qq.com>
2022-08-21 14:01:23 +0800

    compose的主题Theme

---
+++ app/src/main/java/com/hippo/composeUi/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.hippo.composeUi.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFFEFB8C8)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+val TextdarkColor  =Color.White
+val TextlightColor =Color.Black
+
+val Light = Color(0xFFFFFFFF)
+val Dark = Color(0xFF000000)
