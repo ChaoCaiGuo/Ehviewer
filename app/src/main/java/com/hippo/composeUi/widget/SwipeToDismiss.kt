c720e7a0e22cc7525cc79086c686eeb3eb808abe
Guo <heitao_8@qq.com>
2022-09-21 23:35:25 +0800

    增加了自定义了SwipeToDismiss（滑动删除）组件 这只是简单实现，复杂的可以参考Material的实现

---
+++ app/src/main/java/com/hippo/composeUi/widget/SwipeToDismiss.kt
@@ -0,0 +1,86 @@
+package com.hippo.composeUi.widget
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.unit.IntOffset
+import kotlinx.coroutines.delay
+
+@Composable
+fun SwipeToDismiss(
+    modifier: Modifier = Modifier,
+    onDismiss: () -> Unit,
+    dismissThresholds: Float = 0.5f,
+    content: @Composable () -> Unit
+) {
+    var width by remember {
+        mutableStateOf(0)
+    }
+
+    var offset by remember {
+        mutableStateOf(0)
+    }
+
+    var offsetOut by remember {
+        mutableStateOf(false)
+    }
+    LaunchedEffect(offsetOut) {
+        if (offsetOut) {
+            delay(300)
+            onDismiss.invoke()
+        }
+    }
+
+    val animateOffsetWidth by animateIntAsState(targetValue = offset) {
+        if (it == -width) {
+            offsetOut = true
+        }
+    }
+
+    AnimatedVisibility(visible = !offsetOut) {
+        Surface(modifier = modifier
+            .let {
+                if (width == 0)
+                    it.layout { measurable, constraints ->
+                        val placeable = measurable.measure(constraints)
+                        width = placeable.width
+                        layout(placeable.width, placeable.height) {
+                            placeable.placeRelative(0, 0)
+                        }
+                    }
+                else it
+            }
+            .offset {
+                IntOffset(animateOffsetWidth, 0)
+            }
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDragEnd = {
+                        offset = if (offset < -width.toFloat() * dismissThresholds) {
+                            -width
+                        } else {
+                            0
+                        }
+                    }
+                ) { _, dragAmount: Offset ->
+                    offset = (offset + dragAmount.x)
+                        .coerceIn(-width.toFloat(), 0f)
+                        .toInt()
+
+                }
+            }) {
+
+            content.invoke()
+
+        }
+    }
+
+
+}
