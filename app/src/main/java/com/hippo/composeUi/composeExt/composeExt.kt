3d849609668aa6efee78d51f0d1fcae151e8da84
Guo <heitao_8@qq.com>
2022-10-26 13:36:32 +0800

    新增LazyGrid的paging支持

--- app/src/main/java/com/hippo/composeUi/composeExt/composeExt.kt
+++ app/src/main/java/com/hippo/composeUi/composeExt/composeExt.kt
@@ -1,20 +1,23 @@
 package com.hippo.composeUi.composeExt
 
 import android.widget.LinearLayout
+import androidx.compose.foundation.lazy.grid.LazyGridItemScope
+import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
+import androidx.paging.compose.LazyPagingItems
 import com.hippo.composeUi.theme.EhViewerTheme
 
 /**
  *  通过LinearLayout来作为compose的过渡方式
  *  不要直接使用ComposeView，因为直接添加ComposeView 会因为未测量子控件导致ComposeView的wrap_content为0dp
  */
-fun LinearLayout.addComposeView(content: @Composable () -> Unit){
+fun LinearLayout.addComposeView(content: @Composable () -> Unit) {
 
     this.addView(ComposeView(context).apply {
         setContent {
 
-            EhViewerTheme{
+            EhViewerTheme {
 
                 content.invoke()
 
@@ -23,4 +26,18 @@ fun LinearLayout.addComposeView(content: @Composable () -> Unit){
         }
     })
 
+}
+
+fun <T : Any> LazyGridScope.items(
+    items: LazyPagingItems<T>,
+    key: ((index: Int) -> Any)? = null,
+    itemContent: @Composable LazyGridItemScope.(value: T?) -> Unit
+) {
+    items(
+        count = items.itemCount,
+        key = key,
+        itemContent ={ index ->
+            itemContent(items[index])
+        }
+    )
 }
