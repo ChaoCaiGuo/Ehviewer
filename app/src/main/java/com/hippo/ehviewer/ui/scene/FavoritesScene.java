6408fb0717062cddd03dd2ab70cd675da1aa7c3f
tarsin <yuanqingxiang233@163.com>
2022-11-03 15:31:31 +0800

    Code clean and allow refresh pages

--- app/src/main/java/com/hippo/ehviewer/ui/scene/FavoritesScene.java
+++ app/src/main/java/com/hippo/ehviewer/ui/scene/FavoritesScene.java
@@ -1179,7 +1179,7 @@ public class FavoritesScene extends BaseScene implements
 
     private class FavoritesHelper extends GalleryInfoContentHelper {
         public int pgCounter = 0;
-        public String nextPg = "";
+        public String nextPg = null;
 
         @Override
         protected void getPageData(final int taskId, int type, int page) {
@@ -1302,6 +1302,12 @@ public class FavoritesScene extends BaseScene implements
                     mSearchBarMover.showSearchBar();
                 }
             }
+        }
+
+        @Override
+        protected void onClearData() {
+            super.onClearData();
+            nextPg = null;
         }
     }
 }
